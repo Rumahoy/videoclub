073ea960a7d275283e72d63dc9498a40be6ac230
Samuel Fuentes <1833129@alu.murciaeduca.es>
2022-04-26 10:28:36 +0200

    Solucion a los ejercicios 2

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,27 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('login', function () {
+    return "Login usuario";
+});
+
+Route::get('logout', function () {
+    return "Logout usuario";
+});
+
+Route::get('catalog', function () {
+    return "Listado películas";
+});
+
+Route::get('catalog/show/{id}', function ($id) {
+    return "Vista detalle película " . $id;
+});
+
+Route::get('catalog/create', function () {
+    return "Añadir película";
+});
+
+Route::get('catalog/edit/{id]', function ($id) {
+    return "Modificar película " . $id;
+});
