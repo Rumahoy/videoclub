0e9eb19c9d9a316398913c490da95d767457e820
Samuel Fuentes <1833129@alu.murciaeduca.es>
2022-05-03 10:41:03 +0200

    Solucion a los ejercicios 3.1

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\CatalogController;
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -25,18 +27,9 @@ Route::get('logout', function () {
     return view('auth.logout');
 });
 
-Route::get('catalog', function () {
-    return view('catalog.index');
-});
+Route::get('/', [HomeController::class, 'getHome']);
+Route::get('catalog', [CatalogController::class, 'getIndex']);
+Route::get('catalog/show/{id}', [CatalogController::class, 'getShow']);
+Route::get('catalog/create', [CatalogController::class, 'getCreate']);
+Route::get('catalog/edit/{id}', [CatalogController::class, 'getEdit']);
 
-Route::get('catalog/show/{id}', function ($id) {
-    return view('catalog.show', array('id'=>$id));
-});
-
-Route::get('catalog/create', function () {
-    return view('catalog.create');
-});
-
-Route::get('catalog/edit/{id}', function ($id) {
-    return view('catalog.edit', array('id'=>$id));
-});
